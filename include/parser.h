4a5012ed6326bde5e3fe3c5fbc905e3e2e00abc6
Vishal Canumalla <vishalc@cs.washington.edu>
2023-06-26 18:14:42 -0700

    oops forgot this

--- include/parser.h
+++ include/parser.h
@@ -10,11 +10,14 @@ using std::unique_ptr;
 unique_ptr<ExprAST> logError(const char *Str);
 unique_ptr<PrototypeAST> logErrorP(const char *Str);
 static unique_ptr<ExprAST> parseNumberExpr();
-// static unique_ptr<ExprAST> parseExpression();
+static unique_ptr<ExprAST> parseExpression();
 static unique_ptr<ExprAST> parseParenExpr();
 static unique_ptr<ExprAST> parseIdentifierExpr();
 static unique_ptr<ExprAST> parsePrimary();
+static unique_ptr<ExprAST> parseBinOpRHS();
 
 static std::map<char, int> binopPrecedence;
 static int getTokPrecedence();
+
+
 #endif
